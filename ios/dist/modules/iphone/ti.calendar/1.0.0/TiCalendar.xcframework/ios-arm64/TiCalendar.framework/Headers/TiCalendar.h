f91511b7cfaa3b2d162ad02e3f83313a965a306e
hansemannn <hans.knoechel@hs-osnabrueck.de>
2020-11-25 10:52:24 +0100

    chore: move iOS to /ios

---
+++ ios/dist/modules/iphone/ti.calendar/1.0.0/TiCalendar.xcframework/ios-arm64/TiCalendar.framework/Headers/TiCalendar.h
@@ -0,0 +1,17 @@
+//
+//  TiCalendar.h
+//  titanium-calendar
+//
+//  Created by Hans Knoechel
+//  Copyright (c) 2020 Your Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TiCalendar.
+FOUNDATION_EXPORT double TiCalendarVersionNumber;
+
+//! Project version string for TiCalendar.
+FOUNDATION_EXPORT const unsigned char TiCalendarVersionString[];
+
+#import "TiCalendarModuleAssets.h"
