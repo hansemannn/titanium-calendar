1afb1d6d20be96e2052745b0a8b915c699b1dfeb
miga <miga@migamac.local>
2022-01-01 16:32:04 +0100

    feat(ios): add firstDate value

--- ios/Classes/TiCalendarCalendarProxy.m
+++ ios/Classes/TiCalendarCalendarProxy.m
@@ -15,63 +15,65 @@
 {
   if (self = [super _initWithPageContext:context args:args]) {
     ENSURE_SINGLE_ARG(args, NSDictionary);
-  
+
     NSString *title = args[@"title"];
     NSDate *value = args[@"value"];
     NSDate *maxDate = args[@"maxDate"];
+    NSDate *firstDate = args[@"firstDate"];
     UIColor *circleBackgroundColor = args[@"circleBackgroundColor"];
     UIColor *circleSelectedBackgroundColor = args[@"circleSelectedBackgroundColor"];
     UIColor *textColor = args[@"textColor"];
     UIColor *textSelectedColor = args[@"textSelectedColor"];
     UIColor *todayTextColor = args[@"todayTextColor"];
     UIColor *todayCircleBackgroundColor = args[@"todayCircleBackgroundColor"];
-    
+
     _currentCalendar = [[PDTSimpleCalendarViewController alloc] init];
     _currentCalendar.delegate = self;
-    
+
     if (circleBackgroundColor != nil) {
       [[PDTSimpleCalendarViewCell appearance] setCircleDefaultColor:[TiUtils colorValue:circleBackgroundColor].color];
     }
-    
+
     if (circleSelectedBackgroundColor != nil) {
       [[PDTSimpleCalendarViewCell appearance] setCircleSelectedColor:[TiUtils colorValue:circleSelectedBackgroundColor].color];
     }
-    
+
     if (textColor != nil) {
       [[PDTSimpleCalendarViewCell appearance] setTextDefaultColor:[TiUtils colorValue:textColor].color];
     }
-    
+
     if (textSelectedColor != nil) {
       [[PDTSimpleCalendarViewCell appearance] setTextSelectedColor:[TiUtils colorValue:textSelectedColor].color];
     }
-    
+
     if (todayCircleBackgroundColor != nil) {
       [[PDTSimpleCalendarViewCell appearance] setCircleTodayColor:[TiUtils colorValue:todayCircleBackgroundColor].color];
     }
-    
+
     if (todayTextColor != nil) {
       [[PDTSimpleCalendarViewCell appearance] setTextTodayColor:[TiUtils colorValue:todayTextColor].color];
     }
-    
+
     if (title != nil) {
       _currentCalendar.navigationItem.title = title;
     }
-    
+
+    _currentCalendar.firstDate = firstDate ?: [[NSCalendar currentCalendar] dateByAddingUnit:NSCalendarUnitYear value:-3 toDate:[NSDate date] options:0];;
+    _currentCalendar.lastDate = maxDate ?: [[NSCalendar currentCalendar] dateByAddingUnit:NSCalendarUnitYear value:3 toDate:[NSDate date] options:0];;
+
     if (value != nil) {
       _currentCalendar.selectedDate = value;
     }
-
-    _currentCalendar.lastDate = maxDate ?: [[NSCalendar currentCalendar] dateByAddingUnit:NSCalendarUnitYear value:3 toDate:[NSDate date] options:0];;
 
     // Add cancel button
     UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Cancel", @"Cancel")
                                                                      style:UIBarButtonItemStylePlain
                                                                     target:self
                                                                     action:@selector(hide:)];
-    
+
     _currentCalendar.navigationItem.rightBarButtonItem = cancelButton;
   }
-  
+
   return self;
 }
 
@@ -89,7 +91,7 @@
 - (void)show:(id)args
 {
   UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:_currentCalendar];
-  
+
   [TiApp.app showModalController:navigationController animated:true];
   [self fireEvent:@"open"];
 }
@@ -97,7 +99,7 @@
 - (void)hide:(id)unused
 {
   [TiApp.app hideModalController:_currentCalendar.navigationController animated:YES];
-  
+
   [self fireEvent:@"close"];
 }
 
